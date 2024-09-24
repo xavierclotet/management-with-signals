7dfa08af125687aeb789f7f6914fd9447b87a349
xavic <xavi.xavi@gmail.com>
2024-09-24 13:26:10 +0200

    add daisyUi lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'management-with-signals';
+  title = 'Boardgames app';
 }
