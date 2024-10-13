6c537dabea10c3211c7decfe7c77eef52c7cf0bd
xavic <xavi.xavi@gmail.com>
2024-10-13 23:16:42 +0200

    we dont need store to ApplicationConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,11 @@ import { provideRouter, withComponentInputBinding } from "@angular/router";
 
 import { routes } from "./app.routes";
 import { provideHttpClient } from "@angular/common/http";
-import { BoardgamesStore } from "./boardgame/boardgames.store";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(),
-    BoardgamesStore,
   ],
 };
